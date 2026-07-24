9b9facef1becbd25ed9fc2e0a29233479c3cdb39
Nihar Sreeram <nihrob2015@gmail.com>
2026-07-24 14:04:39 +0530

    Add code for database with db file created

---
+++ database.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect('thoughts.db')
+        self.cursor = self.conn.cursor()
+        
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS thoughts(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL,
+            created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+                            """)
+        
+        self.conn.commit()
+        
+    
+    def add_thought(self, text):
+        self.cursor.execute("INSERT INTO thoughts(text) VALUES(?)", (text,))
+        self.conn.commit()
+        
+    def get_thoughts(self):
+        self.cursor.execute("SELECT id, text, created FROM thoughts ORDER BY created DESC")
+        
+        return self.cursor.fetchall()
+    
+    def delete(self, thought_id):
+        self.cursor.execute("DELETE FROM thoughts WHERE id = ?", (thought_id,))
+        self.conn.commit()
+        
+        
