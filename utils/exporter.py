bb61700e82a5468021f8ce05dd5eb4455fce9496
Nihar Sreeram <nihrob2015@gmail.com>
2026-08-05 14:54:10 +0530

    Refactor Exporter methods to include favorite thoughts and associated tags in export outputs

--- utils/exporter.py
+++ utils/exporter.py
@@ -1,12 +1,20 @@
 import json
+from database import Database
 
 class Exporter:
     
     @staticmethod
     def export_txt(path, thoughts):
+        db = Database()
+        
         with open(path, "w", encoding = "utf-8") as file:
-            for _, text, date, _ in thoughts:
-                
+            for id, text, date, favorite in thoughts:
+                if favorite == 1:
+                    file.write("*Favorite Thought*\n\n")
+                tags = db.get_tags(id)
+                for tag in tags:
+                    file.write(f"#{tag} ")
+                file.write("\n")
                 file.write(f"{date}\n")
                 file.write(text + "\n\n")
                 file.write("-" * 50)
@@ -14,10 +22,17 @@ class Exporter:
                 
     @staticmethod
     def export_markdown(path, thoughts):
+        db = Database()
         with open(path, "w", encoding = "utf-8") as file:
             file.write("# ThoughtInbox Export\n\n")
             
-            for _, text, date, _ in thoughts:
+            for id, text, date, favorite in thoughts:
+                if favorite == 1:
+                    file.write("**Favorite Thought**\n\n")
+                tags = db.get_tags(id)
+                for tag in tags:
+                    file.write(f"#{tag} ")
+                file.write("\n")
                 file.write(f"## {date}\n\n")
                 file.write(text)
                 file.write("\n\n---\n\n")
@@ -25,9 +40,11 @@ class Exporter:
     @staticmethod
     def export_json(path, thoughts):
         data = []
+        db = Database()
         
-        for _, text, date, _ in thoughts:
-            data.append({"date": date, "thought": text})
+        for id, text, date, favorite in thoughts:
+            tags = db.get_tags(id)
+            data.append({"date": date, "thought": text, "favorite": bool(favorite), "tags": tags})
             
         with open(path, "w", encoding="utf-8") as file:
             json.dump(data, file, indent = 4, ensure_ascii = False)
